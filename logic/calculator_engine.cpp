071ac93ba66f14b23d6a8bbc6aaf349f014d1429
Aleksey Demchenkov <114291183+Pelfox@users.noreply.github.com>
2025-04-15 22:16:32 +0300

    feat: initial commit

---
+++ logic/calculator_engine.cpp
@@ -0,0 +1,7 @@
+#include "calculator_engine.h"
+
+QString CalculatorEngine::evaluate(const QString& expression)
+{
+    // Dummy implementation for testing
+    return QString("Result of: ") + expression;
+}
---
+++ logic/calculator_engine.h
@@ -0,0 +1,12 @@
+#ifndef CALCULATORENGINE_H
+#define CALCULATORENGINE_H
+
+#include <QString>
+
+class CalculatorEngine
+{
+public:
+    QString evaluate(const QString& expression);
+};
+
+#endif // CALCULATORENGINE_H
