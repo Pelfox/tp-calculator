78738ae8987d7d330d10997acf64804de831dd34
Aleksey Demchenkov <contact@pelfox.dev>
2025-05-21 11:17:32 +0300

    feat(icons): added icons

--- main.cpp
+++ main.cpp
@@ -4,8 +4,14 @@
 int main(int argc, char *argv[]) {
     QApplication app(argc, argv);
     MainWindow window;
+
+    // disable resizing for the window
     window.setWindowFlags(window.windowFlags() & ~Qt::WindowMinimizeButtonHint);
     window.setFixedSize(window.size());
+
+    // set application icon
+    QApplication::setWindowIcon(QIcon(":/icons/icon.png"));
+
     window.show();
     return QApplication::exec();
 }
